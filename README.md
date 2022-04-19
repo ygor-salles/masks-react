354564d346bdeb73b368616123b55f6bd3d7a378
ygor-salles <ygorsallesaniceto@gmail.com>
2022-04-18 23:15:23 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+<p align="center">
+    <img src="public/apresentacao.gif">
+</p>
+
+# 🔖 Sobre
+
+Nesse projeto você poderá ver como criar `input masks` personalizados, de acordo com as necessidades do seu projeto.
+
+Nele utilizamos `regex` para fazer a formatação e validação dos nossos inputs!
+
+## 🗂 Material de apoio
+
+- [Typescript](https://www.typescriptlang.org/)
+- [Typescript no React](https://github.com/typescript-cheatsheets/react-typescript-cheatsheet)
+- [Regex](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/RegExp)
+
+## 👍🏻 Como baixar
+
+```bash
+
+    // Clonar repositório
+    $ git clone https://github.com/danileao/youtube-input-mask.git
+
+    // Acessar diretório
+    $ cd youtube-input-mask
+
+    // Instalar dependências
+    $ yarn
+
+    // Iniciar projeto
+    $ yarn start
+```
+
+---
+
+<p align="center">
+   <a href="https://twitter.com/danieleleao"> <img src="public/twitter.png" width="200"></a>
+    <a href="https://www.instagram.com/dani_leao/"><img src="public/insta.png"  width="200"></a>
+</p>
---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import React, { useState, useCallback } from "react";
+
+import Input from "./components/Input";
+
+import "./App.css";
+
+interface Usuario {
+  cep: string;
+  cpf: string;
+  price: number;
+}
+
+const App: React.FC = () => {
+  const [usuario, setUsuario] = useState<Usuario>({} as Usuario);
+
+  const handleChange = useCallback(
+    (e: React.FormEvent<HTMLInputElement>) => {
+      setUsuario({
+        ...usuario,
+        [e.currentTarget.name]: e.currentTarget.value,
+      });
+    },
+    [usuario]
+  );
+
+  return (
+    <div className="container">
+      <span>CEP</span>
+      <Input
+        name="cep"
+        mask="cep"
+        onChange={handleChange}
+        placeholder="99999-999"
+      />
+
+      <span>CPF</span>
+      <Input
+        name="cpf"
+        mask="cpf"
+        onChange={handleChange}
+        placeholder="999.999.999-99"
+      />
+
+      <span>Preço</span>
+      <Input
+        name="price"
+        mask="currency"
+        prefix="R$"
+        placeholder="0,01"
+        type="text"
+        inputMode="decimal"
+        onChange={handleChange}
+      />
+
+      <button className="button" onClick={() => console.log(usuario)}>
+        Salvar
+      </button>
+    </div>
+  );
+};
+
+export default App;
